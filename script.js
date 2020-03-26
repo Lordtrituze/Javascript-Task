41b2a0b616208dc5adf03816d78bcd1b3da39dce
Lordtrituze <ogunyemiluqman@gmail.com>
2020-03-26 23:57:17 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+ let name = "Luqman Ogunyemi";
+
+ console.log("My name is: " + name);
+
+ var courses = ["FrontEnd", "Design", "Coding"];
+
+ const data = [];
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%3===0) {
+
+     data.push(i);
+
+   }
+
+ }
+
+ console.log(data);
